678ced5e40a6a02a2403e3669691841353bb8083
Victor Martins <vitorchiesa74@gmail.com>
2021-11-07 23:46:45 -0300

    modification 3.5

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,25 @@
 $(document).ready(function(){
 
-    $(".owl-carousel").owlCarousel();
+    $(".owl-carousel").slick({
+
+        variableWidth: true,
+        slidesToShow: 1,
+        arrows: false,
+        dots: true,
+        infinite: true,
+        centerMode: true,
+        centerPadding: "10px",
+        autoplay: true
+    });
 
 });
+
+/*document.querySelector("#items").addEventListener('wheel', event => {
+
+    if(event.deltaY > 0) {
+        event.target.scrollBy(300, 0);
+    } else {
+        event.target.scrollBy(-300, 0);
+    }
+
+});*/
